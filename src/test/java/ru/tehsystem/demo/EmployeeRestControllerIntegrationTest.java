b9d896f4763c5e665df0b5fcdd71ea67b560cc85
Bkmz Litel <bkmzilbim@gmail.com>
2021-04-21 15:22:46 +0500

    Test

--- src/test/java/ru/tehsystem/demo/EmployeeRestControllerIntegrationTest.java
+++ src/test/java/ru/tehsystem/demo/EmployeeRestControllerIntegrationTest.java
@@ -1,16 +1,12 @@
 package ru.tehsystem.demo;
 
 import junit.framework.Assert;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.tehsystem.demo.domain.User;
 import ru.tehsystem.demo.repo.UserRepo;
-
-import java.util.List;
 
 
 @AutoConfigureMockMvc
@@ -26,9 +22,7 @@ public class EmployeeRestControllerIntegrationTest extends Assert {
     @Test
     public void UserTest()
             throws Exception {
-        List<User> all = userRepo.findAll();
-        List<User> all2 = userRepo.findAll();
-        Assertions.assertEquals(all, all2);
+
     }
 
     // write test cases here
